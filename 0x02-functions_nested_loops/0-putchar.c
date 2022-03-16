55998493c48260527f13182311015d6245cf66b6
Joiejoie1 <soniqjjj@gmail.com>
2022-03-16 14:42:04 -0700

    My zero loops task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,34 @@
-#include <stdio.h>
+#include "<stdio.h>
 #include "main.h"
 
 /**
- *  main -Entry point
  *
- *  Description: A program that prints _putchar and new line
+ *  *main - entry point.
  *
- *  Return: returns 0
- **/
+ *   *Description:A program that prints _putchar and new line.
+ *
+ *    *Return: 0 on success.
+ *
+ *     */
 
 int main(void)
+
 {
+
 	char text[9] = "_putchar";
-	int i = 0;
+
+	int i;
 
 	for (i = 0; i < 9; i++)
-	{
-	_putchar(text[i]);
-	}
-	_putchar(*\n*);
 
-	return (0)
-} 
+	{
+
+		_putchar(text[i]);
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+}
