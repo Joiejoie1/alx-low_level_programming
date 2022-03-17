f864acb2289786aa9d5865020fc04a84093c5730
Joy <soniqjjj@gmail.com>
2022-03-16 19:53:49 -0700

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,55 +1,37 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - main block
- *
- *  * Description: Print all possible different combinations of 3 digits.
- *
- *  * Numbers must be separated by commas and a space.
- *
- *  * The 3 digits must be different.
- *
- *  * 012, 120, 102, 021, 201, 210 are considered the same combination.
- *
- *  * print only the smallest combination of 3 digits.
- *
- *  * Numbers should be printed in ascending order.
- *
- *  * You can only use `putchar` to print to console.
- *
- *  * You can only use `putchar` up to 6 times.
- *
- *  * You are not allowed to use any variables of type `char`.
- *
- *  * Return: 0
- *
- *  */
-
+  * main - Prints 3 combination of numbers
+  *
+  * Return: Always (Success)
+  */
 int main(void)
-
 {
-int i, j, k, l;
+	int c, i, k;
 
-for (i = 0; i < 1000; i++)
-{
-j = i / 100; /* hundreds */
-k = (i / 10) % 10; /* tens */
-l = i % 10; /* singles */
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (c < i && i < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(k);
 
-if (j < k && k < l)															{
-putchar(j + '0');
-putchar(k + '0');
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
 
-putchar(l + '0');
+	putchar('\n');
 
-if (i < 700)																																	{																	putchar(44);
-putchar(32);
-}
-}
-}
-
-putchar('\n');
-
-return (0);
+	return (0);
 }
