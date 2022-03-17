3ead411fccf59e119679fc73ac437f070475f312
Joy <soniqjjj@gmail.com>
2022-03-16 19:38:09 -0700

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,28 +1,17 @@
 #include <stdio.h>
 
 /**
+ * main - Prints the alphabetic
  *
- *  * main - main block
- *
- *   * Description: Use `putchar` function to print the alphabet in lowercase.
- *
- *    * Return: 0
- *
- *     */
-
+ * Return: Always (Success)
+ */
 int main(void)
-
 {
-char c = 'a';
+	char c;
 
-while (c <= 'z')
-{
-putchar(c);
-c++;
-}
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 
-putchar('\n');
-
-return (0);
-
+	putchar('\n');
+	return (0);
 }
