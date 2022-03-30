d27b0a865e52fb7db18eea689fa419f31a74a191
Joiejoie1 <soniqjjj@gmail.com>
2022-03-30 00:26:04 -0700

    hot fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,13 @@
 #include "main.h"
 
 /**
-  *
-  *  _memset - fills string with constant char b upto n bytes
-  * 
-  *  @s: input pointer to string
-  *
-  *  @b: constant char
-  *
-  *  @n: number of bytes
-  *
-  *  Return: pointer to s string
-  *
-  */
+ *  _memset - fills string with constant char b upto n bytes 
+ *  @s: input pointer to string
+ *  @b: constant char
+ *  @n: number of bytes
+ *
+ *  Return: pointer to s string
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
