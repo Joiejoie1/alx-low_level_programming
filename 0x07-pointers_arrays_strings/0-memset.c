c078a62f419fe37aedf6f8f82f9bb2d64a8ce127
Joiejoie1 <soniqjjj@gmail.com>
2022-03-30 02:05:35 -0700

    hot fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,13 @@
  */
 
 char *_memset(char *s, char b, unsigned int n)
-{	
-unsigned int i;
-
-for (i = 0; i < n; i++)
 {
-*(s + i) = b;
+unsigned int i = 0;
+
+while (i < n)
+{
+s[i] = b;	
+i++;
 }
 return (s);
 }
